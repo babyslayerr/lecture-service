bbdfa6b79b797b5485d04221eedf5fcafe054f76
정종환 <gksqlcxkdns1@naver.com>
2025-03-20 18:06:32 +0900

    🔨 ADD RepoImpl : 추후 Querydsl 의 확장성을 고려하여 RepoImpl 생성

---
+++ src/main/java/kr/jjh/lecture/lecture/infrastructure/LectureStudentRepositoryImpl.java
@@ -0,0 +1,32 @@
+package kr.jjh.lecture.lecture.infrastructure;
+
+import kr.jjh.lecture.lecture.domain.LectureStudent;
+import kr.jjh.lecture.lecture.domain.LectureStudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+// 추후 Querydsl 추가
+@Repository
+@RequiredArgsConstructor
+public class LectureStudentRepositoryImpl implements LectureStudentRepository {
+
+    private final LectureStudentJpaRepository lectureStudentJpaRepository;
+
+    public LectureStudent save(LectureStudent lectureStudent) {
+        return lectureStudentJpaRepository.save(lectureStudent);
+    }
+
+    public int countByLectureScheduleId(Long lectureScheduleId) {
+
+        return lectureStudentJpaRepository.countByLectureScheduleId(lectureScheduleId);
+    }
+
+    public List<LectureStudent> findAllByLectureScheduleId(long lectureScheduleId) {
+        return lectureStudentJpaRepository.findAllByLectureScheduleId(lectureScheduleId);
+    }
+}
