04d701c218afd7d4068d64d5b3ca4a94b0e866e5
정종환 <gksqlcxkdns1@naver.com>
2025-03-20 18:12:50 +0900

    🔨 ADD PESSIMISTIC : 분산 환경에서의 동시성 고려 메서드 단위 동시성 제어 -> DB 단위 동시성 제어

--- src/main/java/kr/jjh/lecture/lecture/domain/LectureService.java
+++ src/main/java/kr/jjh/lecture/lecture/domain/LectureService.java
@@ -1,7 +1,7 @@
 package kr.jjh.lecture.lecture.domain;
 
-import kr.jjh.lecture.lecture.infrastructure.LectureScheduleRepository;
-import kr.jjh.lecture.lecture.infrastructure.LectureStudentRepository;
+import jakarta.transaction.Transactional;
+import kr.jjh.lecture.lecture.infrastructure.LectureStudentRepositoryImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,18 +9,21 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LectureService {
 
-    private final LectureStudentRepository lectureStudentRepository;
+    private final LectureStudentRepositoryImpl lectureStudentRepository;
     private final LectureScheduleRepository lectureScheduleRepository;
 
-    public synchronized LectureStudent applyLecture(Long studentId, Long lectureScheduleId) {
+    @Transactional
+    public LectureStudent applyLecture(Long studentId, Long lectureScheduleId) {
         // 수강 신청할 강좌
-        LectureSchedule appliedLectureSchedule = lectureScheduleRepository.findById(lectureScheduleId);
+        // 동시성을 고려해서 비관적 락 적용
+        LectureSchedule appliedLectureSchedule = lectureScheduleRepository.findById(lectureScheduleId).orElseThrow();
         // 해당 강좌스케줄을 수강하고 있는 학생수
         int lectureStudentCount = lectureStudentRepository.countByLectureScheduleId(lectureScheduleId);
         if(appliedLectureSchedule.getMax() <= lectureStudentCount){
             throw new IllegalStateException("수강인원을 초과하였습니다.");
         }
 
+        // 수강할 학생 등록
         LectureStudent lectureStudent = new LectureStudent(studentId, lectureScheduleId);
         LectureStudent savedLectureStudent = lectureStudentRepository.save(lectureStudent);
         return savedLectureStudent;
