e21e9f35a68e71def0687caade1ce05a13fed1de
정종환 <gksqlcxkdns1@naver.com>
2025-03-20 18:08:54 +0900

    🔨 ADD entity : Jpa Entity 객체로 변경

--- src/main/java/kr/jjh/lecture/lecture/domain/LectureSchedule.java
+++ src/main/java/kr/jjh/lecture/lecture/domain/LectureSchedule.java
@@ -1,14 +1,20 @@
 package kr.jjh.lecture.lecture.domain;
 
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class LectureSchedule {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne
     @JoinColumn(name = "lecture_id")
